95195d82991d2f744545f146469be02b05bda951
Nabou <Nabou@Sainabou.local>
2018-11-29 14:36:40 -0600

    admin page revamp

--- client/src/components/AdminPage/AdminPage.js
+++ client/src/components/AdminPage/AdminPage.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import React, { Component } from 'react';
+import Table from 'react-materialize';
+import axios from 'axios';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
-const AdminPage = () => (
-    <Admin dataProvider={dataProvider}>
-        <Resource name="users" list={ListGuesser} />
-    </Admin>
-);
- 
+
+class AdminPage extends Component {
+    state = {
+    users: [],
+    }
+
+displayUsers = () => {
+    axios.get('/api/user')
+    .then((response)=> {console.log(response)
+        this.setState({users: response.data})
+    })
+}
+
+componentDidMount(){
+    this.displayUsers()
+}
+
+
+
+
+    render() {
+        return (
+            <div>
+                {this.state.users.map((user, index)=>{
+<td key={`user${index}`}>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th data-field="name">Name</th>
+                            <th data-field="vendorCat">Vendor Category</th>
+                            <th data-field="email">Email</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>{user.name}</td>
+                            <td>{user.vendorCategory}</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        </tbody>
+                </Table>
+
+</td>
+                })
+                
+                }
+                
+            </div>
+        );
+    }
+}
+
 export default AdminPage;
